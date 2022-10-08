974d92b81ac386bcbe8ba197a7c109da5b5ff032
FrostPrice <barbosamat16@gmail.com>
2022-10-07 23:14:46 -0300

    Feat: Add more todos

--- main.cpp
+++ main.cpp
@@ -3,14 +3,17 @@
 // - O jogo deve ter uma função que permite o movimento do personagem pelas posições livres sem deixar rastro - OK
 // - Modifiquem o método de movimentação para o jogador ficar fixo numa posição da tela e o mapa se mover para executar o movimento
 // - Criem um campo de visão para indicar quanto do mapa vai aparecer. se o valor for 3, deve aparecer somente 3 - OK
-// - Façam um menu cin as Opções:
-//  1) Novo Jogo
-//  2) Continuar
-//  3) Salvar
-// - Façam o mapa do jogador 2 ser exibido como uma matriz transposta
-// Colocar 2 jogadores
-// Pontuação
-// Criar um pega-pega com os 2 jogadores
+// Faça um menu com as opções:
+//      - Novo jogo
+//      - Continuar
+//      - Salvar
+// - Faça o mapa do jogador 2 ser exibido de forma transposta em relação ao do jogador 1
+// - Faça o jogador 1 vencer quando o jogador 2 estiver perto (dentro do campo de visão do jogador 1)
+// - Conte o tempo que levou para isso acontecer (tempo de fim - tempo de inicio do jogo)
+// - Inclua um ranking no menu com os melhores tempos ordenados
+// - Faça portais que permitem o jogador aparecer em uma posição aleatória livre no mapa
+// - Faça itens que quando pegos reduzem o campo de visão do adversário por 5 segundos.
+// - Faça um item que quando pego bloqueia o movimento do adversário por 5 segundos.
 #pragma endregion TODOS
 
 using namespace std;
