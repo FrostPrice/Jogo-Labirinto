492fe372f2404ac5a5af46bdfbde06ffbac8ff29
FrostPrice <barbosamat16@gmail.com>
2022-10-13 08:56:17 -0300

    Docs: Improve comments

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #pragma region TODOS
+/*
+--------------------------------------------------------------------------------------------
 // - Faça um programa que lê um arquivo com indicações de espaço livre(0), barreira(1), personagem(2) - OK
 // - O jogo deve ter uma função que permite o movimento do personagem pelas posições livres sem deixar rastro - OK
 // - Modifiquem o método de movimentação para o jogador ficar fixo numa posição da tela e o mapa se mover para executar o movimento - OK
 // - Criem um campo de visão para indicar quanto do mapa vai aparecer. se o valor for 3, deve aparecer somente 3 - OK
-// Faça um menu com as opções:
-//      - Novo jogo
-//      - Continuar
-//      - Salvar
-// - Faça o mapa do jogador 2 ser exibido de forma transposta em relação ao do jogador 1
-// - Faça o jogador 1 vencer quando o jogador 2 estiver perto (dentro do campo de visão do jogador 1)
-// - Conte o tempo que levou para isso acontecer (tempo de fim - tempo de inicio do jogo)
-// - Inclua um ranking no menu com os melhores tempos ordenados
-// - Faça portais que permitem o jogador aparecer em uma posição aleatória livre no mapa
-// - Faça itens que quando pegos reduzem o campo de visão do adversário por 5 segundos.
-// - Faça um item que quando pego bloqueia o movimento do adversário por 5 segundos.
+--------------------------------------------------------------------------------------------
+- Faça um menu com as opções:
+    - Novo jogo
+    - Continuar
+    - Salvar
+- Faça o mapa do jogador 2 ser exibido de forma transposta em relação ao do jogador 1
+- Faça o jogador 1 vencer quando o jogador 2 estiver perto (dentro do campo de visão do jogador 1)
+- Conte o tempo que levou para isso acontecer (tempo de fim - tempo de inicio do jogo)
+- Inclua um ranking no menu com os melhores tempos ordenados
+- Faça portais que permitem o jogador aparecer em uma posição aleatória livre no mapa
+- Faça itens que quando pegos reduzem o campo de visão do adversário por 5 segundos.
+- Faça um item que quando pego bloqueia o movimento do adversário por 5 segundos.
+ */
 #pragma endregion TODOS
 
 using namespace std;
@@ -88,7 +92,7 @@ void show_map(File file, Player player)
                 switch (file.matrix[i][j])
                 {
                 case 0:
-                    mvaddch(win_x, win_y, ' ');
+                    mvaddch(win_x, win_y, ' '); // Move the cursor to the defined x and y, and add a char to that position
                     break;
                 case 1:
                     mvaddch(win_x, win_y, char(219));
